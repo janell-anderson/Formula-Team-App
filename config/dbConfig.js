3a304f7e153e008d58f7407461b628c3109164c4
Janell Anderson <mouse@ool-ad0201ec.dyn.optonline.net>
2018-04-23 19:08:55 -0400

    changed config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,21 @@
 // exporting the postgres port and database chosen
 
-module.exports = {
-  host: 'localhost',
-  port: 5432,
-  database: 'formula_test_db'
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+ const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'formula_test_db'
+  }
+ } else if (process.env.NODE_ENV === 'production') {
+  const config = process.env.DATABASE_URL
 }
+
+
+
+module.exports = config;
+
+// module.exports = {
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'formula_test_db' || 'DATABASE'
+// }
