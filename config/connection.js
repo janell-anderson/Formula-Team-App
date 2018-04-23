cd6bb829b40706f5e007516c77a3c9b5103e5fd0
Janell Anderson <mouse@ool-ad0201ec.dyn.optonline.net>
2018-04-23 19:17:01 -0400

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,16 @@
 const pgp = require('pg-promise')();
 const config = require('./dbConfig');
 
-const db = pgp(config);
+
+
+// const db = pgp(config);
+
+
+  if (process.env.NODE_ENV === 'development' || !proecess.env.NODE_ENV) {
+    const db = pgp(config);
+  } else if (process.env.NODE_ENV === 'production') {
+    const db = pgp(process.env.DATABASE_URL);
+  }
+
 
 module.exports = db;
--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,21 +1,8 @@
 // exporting the postgres port and database chosen
 
-if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
- const config = {
-    host: 'localhost',
-    port: 5432,
-    database: 'formula_test_db'
-  }
- } else if (process.env.NODE_ENV === 'production') {
-  const config = process.env.DATABASE_URL
+
+module.exports = {
+  host: 'localhost',
+  port: 5432,
+  database: 'formula_test_db' || 'DATABASE'
 }
-
-
-
-module.exports = config;
-
-// module.exports = {
-//   host: 'localhost',
-//   port: 5432,
-//   database: 'formula_test_db' || 'DATABASE'
-// }
